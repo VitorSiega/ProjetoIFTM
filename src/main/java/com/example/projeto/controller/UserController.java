a884694d49eccc404366dff1f49e8795f7d9262e
Xnijinv4X <vitorsiega9123@gmail.com>
2024-09-16 15:40:28 -0300

    tratativa de erros de cadastro

--- src/main/java/com/example/projeto/controller/UserController.java
+++ src/main/java/com/example/projeto/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.projeto.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.projeto.dto.CreateUserDTO;
 import com.example.projeto.dto.JwtTokenDTO;
 import com.example.projeto.dto.LoginUserDTO;
+import com.example.projeto.errorStatus.ErrorResponse;
 import com.example.projeto.service.UserService;
 
 @RestController
@@ -21,16 +23,24 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/login")
-    public ResponseEntity<JwtTokenDTO> loginUsuario(@RequestBody LoginUserDTO loginUserDto) {
+public ResponseEntity<?> loginUsuario(@RequestBody LoginUserDTO loginUserDto) {
+    try {
         JwtTokenDTO token = userService.autenticarUsuario(loginUserDto);
         return new ResponseEntity<>(token, HttpStatus.OK);
+    } catch (AuthenticationException e) {
+        // Autenticação falhou, retornar status 401
+        return new ResponseEntity<>(new ErrorResponse("Credenciais inválidas"), HttpStatus.UNAUTHORIZED);
+    } catch (Exception e) {
+        // Outros erros
+        return new ResponseEntity<>(new ErrorResponse("Erro interno do servidor"), HttpStatus.INTERNAL_SERVER_ERROR);
     }
+}
 
     @PostMapping("/cadastro")
     public ResponseEntity<String> cadastrarUsuario(@RequestBody CreateUserDTO createUserDTO) {
         try {
             userService.salvarUsuario(createUserDTO);
-            return ResponseEntity.ok("Usuário cadastrado com sucesso!");
+            return ResponseEntity.status(201).body(null);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao cadastrar usuário: " + e.getMessage());
         }
