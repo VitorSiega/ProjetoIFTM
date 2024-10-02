b304d583cd16eba2a907aea97ca3fcf6ae332ca9
Carlos Eduardo Silva Pires <carlossilvaptu01@gmail.com>
2024-10-01 21:24:32 -0300

    correção metodo put lançar presença

--- src/main/java/com/example/projeto/presenca/controller/PresencaController.java
+++ src/main/java/com/example/projeto/presenca/controller/PresencaController.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,7 +23,7 @@ public class PresencaController {
     @Autowired
     PresencaService presencaService;
 
-    @PostMapping("/admin/lancar/presenca")
+    @PutMapping("/admin/lancar/presenca")
     public ResponseEntity<?> registrarPresenca(@RequestBody List<PresencaDTO> presencaDTO) {
         presencaService.registrarPresenca(presencaDTO);
         return ResponseEntity.status(200).body(null);
