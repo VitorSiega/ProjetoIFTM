79ca9a03171df6e317e7b722acaf4b49583001a6
Xnijinv4X <vitorsiega9123@gmail.com>
2024-10-05 12:28:32 -0300

    adicionando endpoint webhook

--- src/main/java/com/example/projeto/login/webhook/webhookController/WebhookController.java
+++ src/main/java/com/example/projeto/login/webhook/webhookController/WebhookController.java
@@ -16,19 +16,13 @@ public class WebhookController {
 
     private void executeDeployScript() {
         try {
-            // Define o caminho do script
             String scriptPath = "/home/webhook/deploy.sh";
 
-            // Cria um ProcessBuilder para executar o script
             ProcessBuilder processBuilder = new ProcessBuilder("bash", scriptPath);
-            processBuilder.inheritIO(); // Para herdar a entrada e saída do processo
-            Process process = processBuilder.start(); // Inicia o processo
-            process.waitFor(); // Aguarda a conclusão do processo
-
-            // Aqui você pode adicionar qualquer lógica adicional se necessário
+            processBuilder.inheritIO();
+            Process process = processBuilder.start();
+            process.waitFor();
         } catch (IOException | InterruptedException e) {
-            // Tratar exceções conforme necessário
-            e.printStackTrace();
         }
     }
 }
