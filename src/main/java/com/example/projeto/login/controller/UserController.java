a06ede1c60c6505784d431df4fbf9ac5bf23e08d
Xnijinv4X <vitorsiega9123@gmail.com>
2024-10-05 13:37:27 -0300

    adicao error

--- src/main/java/com/example/projeto/login/controller/UserController.java
+++ src/main/java/com/example/projeto/login/controller/UserController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.projeto.login.dto.CreateUserDTO;
-import com.example.projeto.login.dto.JwtTokenDTO;
-import com.example.projeto.login.dto.LoginUserDTO;
-import com.example.projeto.login.errorStatus.ErrorResponse;
 import com.example.projeto.login.model.ModelUser;
 import com.example.projeto.login.repository.UserRepository;
 import com.example.projeto.login.service.UserService;
@@ -33,21 +29,20 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/user/login")
-    public ResponseEntity<?> loginUsuario(@RequestBody LoginUserDTO loginUserDto) {
-        try {
-            JwtTokenDTO token = userService.autenticarUsuario(loginUserDto);
-            return new ResponseEntity<>(token, HttpStatus.OK);
-        } catch (AuthenticationException e) {
-            // Autenticação falhou, retornar status 401
-            return new ResponseEntity<>(new ErrorResponse("Credenciais inválidas"), HttpStatus.UNAUTHORIZED);
-        } catch (Exception e) {
-            // Outros erros
-            return new ResponseEntity<>(new ErrorResponse("Erro interno do servidor"),
-                    HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
+    // @PostMapping("/user/login")
+    // public ResponseEntity<?> loginUsuario(@RequestBody LoginUserDTO loginUserDto) {
+    //     try {
+    //         JwtTokenDTO token = userService.autenticarUsuario(loginUserDto);
+    //         return new ResponseEntity<>(token, HttpStatus.OK);
+    //     } catch (AuthenticationException e) {
+    //         // Autenticação falhou, retornar status 401
+    //         return new ResponseEntity<>(new ErrorResponse("Credenciais inválidas"), HttpStatus.UNAUTHORIZED);
+    //     } catch (Exception e) {
+    //         // Outros erros
+    //         return new ResponseEntity<>(new ErrorResponse("Erro interno do servidor"),
+    //                 HttpStatus.INTERNAL_SERVER_ERROR);
+    //     }
+    // }
     @PostMapping("/admin/cadastro")
     public ResponseEntity<String> cadastrarUsuario(@RequestBody CreateUserDTO createUserDTO) {
         try {
