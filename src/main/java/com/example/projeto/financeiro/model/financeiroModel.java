0c9fbfa9b563bb758fbf3821ac72b2bd9137c35b
Xnijinv4X <vitorsiega9123@gmail.com>
2024-10-07 16:44:39 -0300

    add fuction financeiro

---
+++ src/main/java/com/example/projeto/financeiro/model/financeiroModel.java
@@ -0,0 +1,49 @@
+package com.example.projeto.financeiro.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import com.example.projeto.login.model.ModelUser;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Table(name = "financeiro")
+@Entity
+public class financeiroModel implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "operador_id", nullable = false)
+    private ModelUser user;
+
+    @Column(name = "mes", nullable = false)
+    private int mesAtual;
+
+    @Column(name = "dia_pago", nullable = false, columnDefinition = "date")
+    private LocalDate diaPago;
+
+    @Column(name = "valor_pago", nullable = false)
+    Float valorPago;
+
+    @Column(name = "status", nullable = false, columnDefinition = "varchar(20)")
+    private String status;
+}
