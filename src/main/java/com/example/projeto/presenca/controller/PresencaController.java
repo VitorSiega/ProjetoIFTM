da8096ce327d4a0310275b46cd9f5c1c372c042a
Xnijinv4X <vitorsiega9123@gmail.com>
2024-10-21 11:56:50 -0300

    teste

--- src/main/java/com/example/projeto/presenca/controller/PresencaController.java
+++ src/main/java/com/example/projeto/presenca/controller/PresencaController.java
@@ -6,13 +6,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.projeto.presenca.dto.PresencaDTO;
 import com.example.projeto.presenca.model.PresencaModel;
 import com.example.projeto.presenca.service.PresencaService;
 
@@ -23,11 +20,12 @@ public class PresencaController {
     @Autowired
     PresencaService presencaService;
 
-    @PutMapping("/lancar")
-    public ResponseEntity<?> registrarPresenca(@RequestBody List<PresencaDTO> presencaDTO) {
-        presencaService.registrarPresenca(presencaDTO);
-        return ResponseEntity.status(200).body(null);
-    }
+    // @PutMapping("/lancar")
+    // public ResponseEntity<?> registrarPresenca(@RequestBody List<PresencaDTO>
+    // presencaDTO) {
+    // presencaService.registrarPresenca(presencaDTO);
+    // return ResponseEntity.status(200).body(null);
+    // }
 
     @GetMapping("/listar")
     public ResponseEntity<List<PresencaModel>> listarPresencaPorData(
