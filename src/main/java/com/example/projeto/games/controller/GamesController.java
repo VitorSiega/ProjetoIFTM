64addd03b55b68e01779b2a3b66033993cae6165
Xnijinv4X <vitorsiega9123@gmail.com>
2024-10-22 07:57:36 -0300

    teste webhook

--- src/main/java/com/example/projeto/games/controller/GamesController.java
+++ src/main/java/com/example/projeto/games/controller/GamesController.java
@@ -1,77 +1,85 @@
-package com.example.projeto.games.controller;
+// package com.example.projeto.games.controller;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.example.projeto.games.dto.GamesDTO;
-import com.example.projeto.games.model.GamesModel;
-import com.example.projeto.games.repository.GamesRepository;
-import com.example.projeto.games.service.GamesService;
+// import com.example.projeto.games.dto.GamesDTO;
+// import com.example.projeto.games.model.GamesModel;
+// import com.example.projeto.games.repository.GamesRepository;
+// import com.example.projeto.games.service.GamesService;
 
-@RestController
-@RequestMapping("/api/admin/jogo")
-public class GamesController {
+// @RestController
+// @RequestMapping("/api/admin/jogo")
+// public class GamesController {
 
-    @Autowired
-    GamesService gamesService;
+// @Autowired
+// GamesService gamesService;
 
-    @Autowired
-    GamesRepository gamesRepository;
+// @Autowired
+// GamesRepository gamesRepository;
 
-    @PostMapping("/cadastrar")
-    public ResponseEntity<String> cadastrarJogo(@RequestBody GamesDTO gamesDTO) {
-        try {
-            if (gamesDTO.nome().isEmpty() || gamesDTO.descricao().isEmpty()
-                    || gamesDTO.local().isEmpty() || gamesDTO.responsaveis().isEmpty()) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Preencha todos os dados");
-            }
+// @PostMapping("/cadastrar")
+// public ResponseEntity<String> cadastrarJogo(@RequestBody GamesDTO gamesDTO) {
+// try {
+// if (gamesDTO.nome().isEmpty() || gamesDTO.descricao().isEmpty()
+// || gamesDTO.local().isEmpty() || gamesDTO.responsaveis().isEmpty()) {
+// return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Preencha todos os
+// dados");
+// }
 
-            gamesService.salvarJogo(gamesDTO);
-            return ResponseEntity.status(201).body(null);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao cadastrar jogo: " + e.getMessage());
-        }
-    }
+// gamesService.salvarJogo(gamesDTO);
+// return ResponseEntity.status(201).body(null);
+// } catch (Exception e) {
+// return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao cadastrar
+// jogo: " + e.getMessage());
+// }
+// }
 
-    @PutMapping("/atualizar/{id}")
-    public ResponseEntity<String> atualizarJogo(@PathVariable Long id, @RequestBody GamesDTO gamesDto) {
-        try {
-            if (gamesRepository.findById(id).isEmpty()) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Jogo não encontrado");
-            }
-            if (gamesDto.nome().isEmpty() || gamesDto.descricao().isEmpty() || gamesDto.local().isEmpty() || gamesDto.responsaveis().isEmpty() || gamesDto.situacao().isEmpty()) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Preencha todos os dados");
-            }
-            gamesService.atualizarJogo(id, gamesDto);
-            return ResponseEntity.status(200).body(null);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao alterar jogo: " + e.getMessage());
-        }
-    }
+// @PutMapping("/atualizar/{id}")
+// public ResponseEntity<String> atualizarJogo(@PathVariable Long id,
+// @RequestBody GamesDTO gamesDto) {
+// try {
+// if (gamesRepository.findById(id).isEmpty()) {
+// return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Jogo não
+// encontrado");
+// }
+// if (gamesDto.nome().isEmpty() || gamesDto.descricao().isEmpty() ||
+// gamesDto.local().isEmpty() || gamesDto.responsaveis().isEmpty() ||
+// gamesDto.situacao().isEmpty()) {
+// return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Preencha todos os
+// dados");
+// }
+// gamesService.atualizarJogo(id, gamesDto);
+// return ResponseEntity.status(200).body(null);
+// } catch (Exception e) {
+// return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao alterar
+// jogo: " + e.getMessage());
+// }
+// }
 
-    @GetMapping("/listar")
-    public ResponseEntity<List<GamesModel>> listaJogos() {
-        return ResponseEntity.status(200).body(gamesService.listarJogos());
-    }
+// @GetMapping("/listar")
+// public ResponseEntity<List<GamesModel>> listaJogos() {
+// return ResponseEntity.status(200).body(gamesService.listarJogos());
+// }
 
-    @DeleteMapping("/remover/{id}")
-    public ResponseEntity<String> removerUsuario(@PathVariable Long id) {
-        if (gamesService.removerJogo(id)) {
-            return ResponseEntity.status(200).body(null);
-        } else {
-            return ResponseEntity.status(500).body("Jogo não encontrado");
-        }
-    }
+// @DeleteMapping("/remover/{id}")
+// public ResponseEntity<String> removerUsuario(@PathVariable Long id) {
+// if (gamesService.removerJogo(id)) {
+// return ResponseEntity.status(200).body(null);
+// } else {
+// return ResponseEntity.status(500).body("Jogo não encontrado");
+// }
+// }
 
-}
+// }
--- src/main/java/com/example/projeto/games/service/GamesService.java
+++ src/main/java/com/example/projeto/games/service/GamesService.java
@@ -1,66 +1,66 @@
-package com.example.projeto.games.service;
+// package com.example.projeto.games.service;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Lazy;
+// import org.springframework.stereotype.Service;
 
-import com.example.projeto.games.dto.GamesDTO;
-import com.example.projeto.games.model.GamesModel;
-import com.example.projeto.games.repository.GamesRepository;
+// import com.example.projeto.games.dto.GamesDTO;
+// import com.example.projeto.games.model.GamesModel;
+// import com.example.projeto.games.repository.GamesRepository;
 
-import jakarta.persistence.EntityNotFoundException;
+// import jakarta.persistence.EntityNotFoundException;
 
-@Service
-public class GamesService {
+// @Service
+// public class GamesService {
 
-    @Autowired
-    @Lazy
-    private final GamesRepository gamesRepository;
+// @Autowired
+// @Lazy
+// private final GamesRepository gamesRepository;
 
-    public GamesService(@Lazy GamesRepository gamesRepository) {
-        this.gamesRepository = gamesRepository;
-    }
+// public GamesService(@Lazy GamesRepository gamesRepository) {
+// this.gamesRepository = gamesRepository;
+// }
 
-    public void salvarJogo(GamesDTO createGamesDTO) {
+// public void salvarJogo(GamesDTO createGamesDTO) {
 
-    // // Cria o novo game
-    // GamesModel newGame = GamesModel.builder()
-    // .id(createGamesDTO.id())
-    // .nome(createGamesDTO.nome())
-    // .dataJogo(createGamesDTO.dataJogo())
-    // .descricao(createGamesDTO.descricao())
-    // .local(createGamesDTO.local())
-    // .situacao(createGamesDTO.situacao())
-    // .responsaveis(createGamesDTO.responsaveis())
-    // .build();
-    // gamesRepository.save(newGame);
-    // }
+// // Cria o novo game
+// GamesModel newGame = GamesModel.builder()
+// .id(createGamesDTO.id())
+// .nome(createGamesDTO.nome())
+// .dataJogo(createGamesDTO.dataJogo())
+// .descricao(createGamesDTO.descricao())
+// .local(createGamesDTO.local())
+// .situacao(createGamesDTO.situacao())
+// .responsaveis(createGamesDTO.responsaveis())
+// .build();
+// gamesRepository.save(newGame);
+// }
 
-    public void atualizarJogo(Long id, GamesDTO updateGamesDTO) {
-        GamesModel game = gamesRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Jogo não encontrado!"));
+// public void atualizarJogo(Long id, GamesDTO updateGamesDTO) {
+// GamesModel game = gamesRepository.findById(id)
+// .orElseThrow(() -> new EntityNotFoundException("Jogo não encontrado!"));
 
-        game.setNome(updateGamesDTO.nome());
-        game.setDataJogo(updateGamesDTO.dataJogo());
-        game.setLocal(updateGamesDTO.local());
-        game.setDescricao(updateGamesDTO.descricao());
-        game.setSituacao(updateGamesDTO.situacao());
-        game.setResponsaveis(updateGamesDTO.responsaveis());
+// game.setNome(updateGamesDTO.nome());
+// game.setDataJogo(updateGamesDTO.dataJogo());
+// game.setLocal(updateGamesDTO.local());
+// game.setDescricao(updateGamesDTO.descricao());
+// game.setSituacao(updateGamesDTO.situacao());
+// game.setResponsaveis(updateGamesDTO.responsaveis());
 
-        gamesRepository.save(game);
-    }
+// gamesRepository.save(game);
+// }
 
-    public List<GamesModel> listarJogos() {// retirar depois
-        return gamesRepository.findAll();
-    }
+// public List<GamesModel> listarJogos() {// retirar depois
+// return gamesRepository.findAll();
+// }
 
-    public boolean removerJogo(Long id) {
-        if (gamesRepository.findById(id).isPresent()) {
-            gamesRepository.deleteById(id);
-            return true;
-        }
-        return false;
-    }
-}
+// public boolean removerJogo(Long id) {
+// if (gamesRepository.findById(id).isPresent()) {
+// gamesRepository.deleteById(id);
+// return true;
+// }
+// return false;
+// }
+// }
